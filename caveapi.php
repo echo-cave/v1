a95efd0426fcd27455a86394e3516223e2c26c1e
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2021-08-24 10:28:20 +0800

    Add files via upload

---
+++ caveapi.php
@@ -0,0 +1,18 @@
+﻿<?php
+date_default_timezone_set('Asia/Shanghai');
+
+$jieshaos=file("cave.txt");
+$jieshaoc=count($jieshaos);
+$jieshaor=rand(0,$jieshaoc-1);
+$jieshao=@$jieshaos[$jieshaor];
+$text=str_replace("\n","",$jieshao);
+$text=str_replace("\r","",$text);
+$text=str_replace("\r\n","",$text);
+$jieshao=$text;
+if($_GET["encode"] == "js")
+{
+echo '(function cave(){var cave="回声洞 - ' . $jieshao . '";var dom=document.querySelector(".cave");Array.isArray(dom)?dom[0].innerText=cave:dom.innerText=cave;})()';
+}else{
+echo "$jieshao";
+}
+?>
---
+++ caveup.php
@@ -0,0 +1,9 @@
+﻿<?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://cdn.jsdelivr.net/gh/echo-cave/cave@main/cave.txt");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$st = curl_exec($ch);   //将curl的结果存到变量里
+$fd = fopen("cave.txt", 'w');
+fwrite($fd, $st);  //将curl的结果写入文件里
+fclose($fd);
+curl_close($ch);
