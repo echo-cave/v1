c6b4e0a6201b6781ff95009253f022b95d57df21
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2021-08-24 12:19:03 +0800

    Update caveapi.php

--- caveapi.php
+++ caveapi.php
@@ -12,6 +12,11 @@ $jieshao=$text;
 if($_GET["encode"] == "js")
 {
 echo '(function cave(){var cave="' . $jieshao . '";var dom=document.querySelector(".cave");Array.isArray(dom)?dom[0].innerText=cave:dom.innerText=cave;})()';
+}
+else if($_GET["encode"] == "json")
+{
+$arr = array('code' => 0, 'cave' => $jieshao);
+echo json_encode($arr);
 }else{
 echo "$jieshao";
 }
